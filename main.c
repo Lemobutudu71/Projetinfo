1a0b7f75d08edb471a1243fecccb9b5c689f1fd9
linapereiraalaoui <168921349+linapereiraalaoui@users.noreply.github.com>
2024-05-17 00:45:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,34 @@ int main(){
     placerCibles(grille);
     placerRobots(grille);
     afficherGrille(grille);
+
+    Position robot, cible;
+    choisirRobotCible(grille, &robot, &cible);
+   
+    int niveau_difficulte = 2;
+    int duree_chrono;
+    switch (niveau_difficulte) {
+        case 1:
+            duree_chrono = 10;
+            break;
+        case 2:
+            duree_chrono = 20;
+            break;
+        case 3:
+            duree_chrono = 30;
+            break;
+        default:
+            duree_chrono = 20;
+    }
+    chronometrer(duree_chrono);
+    
+    demanderMouvements(grille, &robot);
+
     freeGrille(grille);
+
+    return 0;
+}
+
 
 
 
