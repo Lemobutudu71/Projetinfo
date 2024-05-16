3bdb2090cae612545c5608ab584358db5f4d8f87
linapereiraalaoui <168921349+linapereiraalaoui@users.noreply.github.com>
2024-05-17 00:52:14 +0200

    Update interface.c

--- interface.c
+++ interface.c
@@ -16,25 +16,22 @@ int nb_joueur(int nb_joueur){ // demande le nombre de joueur
 
 void choisirRobotCible(char grille[][20], Position *robot, Position *cible) {
     int x, y;
-    // Choisir un robot aléatoirement
     do {
-        x = rand() % 18 + 1; // Génère un nombre aléatoire entre 1 et 18
+        x = rand() % 18 + 1; 
         y = rand() % 18 + 1;
-    } while (grille[y][x] != ' '); // Vérifie si la case est vide
+    } while (grille[y][x] != ' '); 
     robot->x = x;
     robot->y = y;
     grille[y][x] = 'R'; // Marque la position du robot sur la grille
 
-    // Choisir une cible aléatoirement
-    do {
+    do { 
         x = rand() % 18 + 1;
         y = rand() % 18 + 1;
     } while (grille[y][x] != ' ');
     cible->x = x;
     cible->y = y;
-    grille[y][x] = 'C'; // Marque la position de la cible sur la grille
+    grille[y][x] = 'C'; 
 
-    // Afficher la grille avec le robot et la cible
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 20; j++) {
             printf("%c ", grille[i][j]);
@@ -44,7 +41,7 @@ void choisirRobotCible(char grille[][20], Position *robot, Position *cible) {
 }
 
 void chronometrer(int duree_chrono) {
-    printf("Le chronomètre est lancé pendant %d secondes...\n", duree_chrono);
+    printf("Le chronomètre est lancé pendant %d secondes\n", duree_chrono);
     sleep(duree_chrono);
 }
 
@@ -53,17 +50,14 @@ void demanderMouvements(char grille[][20], Position *robot) {
     printf("Entrez les mouvements nécessaires (dx dy) : ");
     scanf("%d %d", &dx, &dy);
 
-    // Vérifier si le mouvement demandé est valide
     int nouvelle_x = robot->x + dx;
     int nouvelle_y = robot->y + dy;
     if (nouvelle_x >= 1 && nouvelle_x <= 18 && nouvelle_y >= 1 && nouvelle_y <= 18 && grille[nouvelle_y][nouvelle_x] != '|' && grille[nouvelle_y][nouvelle_x] != '-' && grille[nouvelle_y][nouvelle_x] != 'R') {
-        // Le mouvement est valide, mettre à jour la position du robot
         robot->x = nouvelle_x;
         robot->y = nouvelle_y;
     } else {
-        // Le mouvement est invalide, informer l'utilisateur et redemander les mouvements
-        printf("Mouvement invalide. Assurez-vous de ne pas traverser de murs ou d'autres robots.\n");
-        demanderMouvements(grille, robot); // Appel récursif pour redemander les mouvements
+        printf("Mouvement invalide. IL ne pas traverser de murs ou d'autres robots.\n");
+        demanderMouvements(grille, robot); 
     }
 }
 
