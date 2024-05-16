ebb45e8f4e4e66661c16496f1ec86b1da152a833
Clab-ewan <clabautewa@cy-tech.fr>
2024-05-16 15:52:44 +0200

    Update interface.c
    il se joue au moins à 2 le jeu bande de neuille

--- interface.c
+++ interface.c
@@ -5,8 +5,8 @@
 int nb_joueur(int nb_joueur){ // demande le nombre de joueur
     printf("Veuillez saisir le nombre de joueur : /n ");
     scanf("%d", &nb_joueur);
-    while(nb_joueur <= 0){
-        printf("Le nombre de joueur doit etre superieur a 0 /n");
+    while(nb_joueur <= 1){
+        printf("Le nombre de joueur doit etre superieur a 1 /n");
         scanf("%d", &nb_joueur);
     }
     return nb_joueur;
