329312894e3cba9383e2a0438510e110a05d3a46
linapereiraalaoui <168921349+linapereiraalaoui@users.noreply.github.com>
2024-05-17 00:32:18 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -5,5 +5,8 @@
     
    int nb_joueur(int nb_joueur);
    int choisir_niveau_difficulte(); // Pour choisir le niveau
+   void chronometrer(int duree_chrono); // Chronométrer
+   void demanderMouvements(Grille *grille, Position *robot);
+
     
 #endif
