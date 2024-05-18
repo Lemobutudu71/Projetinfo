6ce0954da0fa56137d0ccd3353dcd84d15827837
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-18 18:28:44 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -7,7 +7,6 @@
     #include <unistd.h>
     
    int nb_joueur(int nb_joueur);
-   int choisir_niveau_difficulte(); // Pour choisir le niveau
    void chronometrer(int duree_chrono); // Chronométrer
    void demanderMouvements(Grille *grille, Position *robot);
 
