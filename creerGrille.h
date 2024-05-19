2396c3de8910e55969c1f9e96691e237fcb20441
Clab-ewan <clabautewa@cy-tech.fr>
2024-05-19 12:39:25 +0200

    Update creerGrille.h

--- creerGrille.h
+++ creerGrille.h
@@ -5,8 +5,12 @@
     #include <stdio.h>
     #include <time.h>
 
-
+    
     char **grille;
+    int MurRandH[4];
+    int MurRandV[4];
+    int CordCibles[CIBLES][2];
+    int AngleDroit[CIBLES][2];
     char **initialiserGrille(char **grille, int hauteur, int largeur);
     void placerCibles(char **grille, int hauteur, int largeur);
     void placerRobots(char **grille, int hauteur, int largeur);
