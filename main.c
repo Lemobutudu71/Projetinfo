80c4343568dfa814783a67d83f2f83a142c891a0
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-21 15:01:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 #include "interface.h"
 #include "creerGrille.h"
 
-// Déclarer les variables globales
-int hauteur,
-largeur;
+int main(){
+
+int largeur, hauteur;
 char **grille;
 int **MurInterdit;
 int MurRandH[4];
@@ -16,52 +16,24 @@ int CordCibles[CIBLES][2];
 int MurH_Cibles[CIBLES][2];
 int MurV_Cibles[CIBLES][2];
 
-
-int main(){
     srand(time(NULL));
-    MurRandH[4];
-    MurRandV[4];
     hauteur = rand() % 6 + 15; // Génère un nombre entre 15 et 20
     largeur = rand() % 6 + 15;  // Génère un nombre entre 15 et 20
     for(int i=0; i<4; i++){
             MurRandH[i]=rand() % (hauteur - 2) + 1; 
             MurRandV[i]=rand() % (largeur - 2) + 1;
-            }
-    initialiserGrille(grille, hauteur, largeur);
-    placerCibles(grille, hauteur, largeur);
+    }
+            
+    initialiserGrille(&grille, hauteur, largeur);      
+    placerCibles(grille, hauteur, largeur, CordCibles, MurH_Cibles, MurV_Cibles);
+    afficherGrille(grille, hauteur, largeur, MurRandH, MurRandV,MurH_Cibles, MurV_Cibles);
     placerRobots(grille, hauteur, largeur);
-
-    afficherGrille(grille, hauteur, largeur);
-    
+	placerCibles(grille, hauteur, largeur, CordCibles, MurH_Cibles, MurV_Cibles);
     char robot, cible;
     choisirRobotCible(grille, hauteur, largeur, &robot, &cible);
     int niveau_difficulte = 2;
-    int duree_chrono;
-    printf("Veuillez choisir le niveau de difficulté entre 1 et 4\n");
-    scanf("%d", &niveau_difficulte);
-    while(niveau_difficulte < 1 || niveau_difficulte > 4){
-        printf("Le niveau de difficulte doit etre compris entre 1 et 4 \n");
-        scanf("%d", &niveau_difficulte);
-    }
-    switch (niveau_difficulte) {
-        case 1:
-            duree_chrono = 10;
-            break;
-        case 2:
-            duree_chrono = 20;
-            break;
-        case 3:
-            duree_chrono = 30;
-            break;
-        default:
-            duree_chrono = 20;
-    }
-    chronometrer(duree_chrono);
-    
-    demanderMouvements(grille, &robot);
+    chronometrer(choixdifficulte(niveau_difficulte));
 
-
-    
     for (int i = 0; i < hauteur; i++) {
         free(grille[i]);
     }
@@ -73,6 +45,4 @@ int main(){
     free(MurInterdit);
 
 return 0;
-
-    
 }
