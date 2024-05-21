76d95ffe108bee71958e6b17b374715f711b4548
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-21 14:58:59 +0200

    Update creerGrille.h

--- creerGrille.h
+++ creerGrille.h
@@ -4,19 +4,18 @@
     #include <stdlib.h>
     #include <stdio.h>
     #include <time.h>
+    #include "couleur.h"
+    #define ROBOTS 4
+    #define CIBLES 18
 
-    
-    char **grille;
-    int MurRandH[4];
-    int MurRandV[4];
-    int CordCibles[CIBLES][2];
-    int AngleDroit[CIBLES][2];
-    char **initialiserGrille(char **grille, int hauteur, int largeur);
-    void placerCibles(char **grille, int hauteur, int largeur);
-    void placerRobots(char **grille, int hauteur, int largeur);
 
-    void afficherGrille(char **grille, int hauteur, int largeur);
+void initialiserGrille(char ***grille, int hauteur, int largeur);
+void initialiserMurInterdit(int ***MurInterdit, int hauteur, int largeur);
+void placerCibles(char **grille, int hauteur, int largeur, int CordCibles[CIBLES][2], int MurH_Cibles[CIBLES][2], int MurV_Cibles[CIBLES][2]);
+void placerRobots(char **grille, int hauteur, int largeur);
+void afficherGrille(char **grille, int hauteur, int largeur, int MurRandH[4],
+int MurRandV[4],int MurH_Cibles[CIBLES][2], int MurV_Cibles[CIBLES][2]);
+int estPositionValide(char **grille, int hauteur, int largeur, int ligne, int col);
 
     
 #endif
-
