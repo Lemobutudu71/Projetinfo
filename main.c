f99b910c0f5be4deb3934212e5a467432c0da8f0
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-24 16:54:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,36 @@
 #include "creerGrille.h"
 #include <stdio.h>
-
 #include <time.h>
-
 #include "interface.h"
 
 
 int main(){
 
- MurInterdit *murInterdits = NULL;
+MurInterdit *murInterdits = NULL;
 int largeur, hauteur;
 char **grille;
-int MurRandH[4];
-int MurRandV[4];
+
+
 int CordCibles[CIBLES][2];
 int MurH_Cibles[CIBLES][2];
 int MurV_Cibles[CIBLES][2];
 int nombreMursInterdits = 0;
+
+    int *MurRandH ;
+    int *MurRandV ;
+
     srand(time(NULL));
     hauteur = rand() % 6 + 15; // Génère un nombre entre 15 et 20
     largeur = rand() % 6 + 15;  // Génère un nombre entre 15 et 20
-    for(int i=0; i<4; i++){
-            MurRandH[i]=rand() % (hauteur - 2) + 1; 
-            MurRandV[i]=rand() % (largeur - 2) + 1;
-    }
+   
             
     initialiserGrille(&grille, hauteur, largeur);
     initialiserMurInterdit(&murInterdits, hauteur, largeur);  
         placerCibles(grille, hauteur, largeur, CordCibles, MurH_Cibles, MurV_Cibles, &murInterdits, &nombreMursInterdits);
             placerRobots(grille, hauteur, largeur, murInterdits, nombreMursInterdits);
-
-            murExterieur(hauteur, largeur, &murInterdits, &nombreMursInterdits, MurRandH, MurRandV);
-            afficherGrille(grille, hauteur, largeur, MurRandV, MurRandH, MurH_Cibles, MurV_Cibles);
+   
+            murExterieur(hauteur, largeur, &murInterdits, &nombreMursInterdits, &MurRandH, &MurRandV);
+            afficherGrille(grille, hauteur, largeur, &MurRandV, &MurRandH, MurH_Cibles, MurV_Cibles);
         
         printf("Coordonnées des murs interdits :\n");
             for (int i = 0; i < nombreMursInterdits; i++) {
@@ -48,11 +47,12 @@ int nombreMursInterdits = 0;
 
         for (int i = 0; i < hauteur; i++) {
                 free(grille[i]);
-        
+               
             }
             free(grille);
-        
             free(murInterdits);
+            free(MurRandH);
+            free(MurRandV);
 
 return 0;
 }
