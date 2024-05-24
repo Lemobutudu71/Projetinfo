f9020c5133f76ea96ba03379f63a73e3dc071e9d
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-24 16:54:04 +0200

    Update creerGrille.h

--- creerGrille.h
+++ creerGrille.h
@@ -14,12 +14,12 @@ typedef struct {
 }MurInterdit;
 
 void initialiserGrille(char ***grille, int hauteur, int largeur);
-void initialiserMurInterdit(int ***MurInterdit, int hauteur, int largeur);
+void initialiserMurInterdit(MurInterdit **murInterdits, int hauteur, int largeur);
 void placerCibles(char **grille, int hauteur, int largeur, int CordCibles[][2], int MurH_Cibles[][2], int MurV_Cibles[][2], MurInterdit **murInterdits, int *nombreMursInterdits);
 void placerRobots(char **grille, int hauteur, int largeur, MurInterdit *murInterdits, int nombreMursInterdits); 
-void afficherGrille(char **grille, int hauteur, int largeur, int MurRandH[4],
-int MurRandV[4],int MurH_Cibles[CIBLES][2], int MurV_Cibles[CIBLES][2]);
+void afficherGrille(char **grille, int hauteur, int largeur, int **MurRandH,
+int **MurRandV,int MurH_Cibles[CIBLES][2], int MurV_Cibles[CIBLES][2]);
 int estPositionValide(char **grille, int hauteur, int largeur, int ligne, int col, MurInterdit *murInterdits, int nombreMursInterdits);
-void murExterieur(int hauteur, int largeur, MurInterdit **murInterdits, int *nombreMursInterdits, int MurRandH[4], int MurRandV[4]);
+void murExterieur(int hauteur, int largeur, MurInterdit **murInterdits, int *nombreMursInterdits, int **MurRandH, int **MurRandV);
     
 #endif
