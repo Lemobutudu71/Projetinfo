b7c0f73eb8293aec252f45727cb7a7cf3c037e11
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-24 19:37:30 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -6,11 +6,18 @@
     #include <time.h>
     #include <unistd.h>
 	#include "couleur.h"
+
+typedef struct{
+    int nbMouv;
+    int points;
+}Joueur;
     
    int NombreJoueurs(int nb_joueur);
    void choisirRobotCible(char **grille, int hauteur, int largeur, char *robot, char *cible);
    void chronometrer(int duree_chrono); // Chronométrer
    int choixdifficulte(int niveau_difficulte);
 
+   void nombresMouv(int nb_joueur, Joueur **joueur);
     
 #endif
+
