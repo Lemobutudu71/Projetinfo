a6d8c1213279dda2017b497cc9653740ba9aa93e
Clab-ewan <clabautewa@cy-tech.fr>
2024-05-25 03:01:58 +0200

    Update creerGrille.h

--- creerGrille.h
+++ creerGrille.h
@@ -1,25 +1,37 @@
 #ifndef Grille_h
-    #define Grille_h
+#define Grille_h
 
-    #include <stdlib.h>
-    #include <stdio.h>
-    #include <time.h>
-    #include "couleur.h"
-    #define ROBOTS 4
-    #define CIBLES 18
+#include "couleur.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ROBOTS 4
+#define CIBLES 18
 
 typedef struct {
-    int ligne;
-    int col;
-}MurInterdit;
+  int ligne;
+  int col;
+} MurInterdit;
+
+void clearInputBuffer();
 
 void initialiserGrille(char ***grille, int hauteur, int largeur);
-void initialiserMurInterdit(MurInterdit **murInterdits, int hauteur, int largeur);
-void placerCibles(char **grille, int hauteur, int largeur, int CordCibles[][2], int MurH_Cibles[][2], int MurV_Cibles[][2], MurInterdit **murInterdits, int *nombreMursInterdits);
-void placerRobots(char **grille, int hauteur, int largeur, MurInterdit *murInterdits, int nombreMursInterdits); 
+
+void placerCibles(char **grille, int hauteur, int largeur, int CordCibles[][2],
+                  int MurH_Cibles[][2], int MurV_Cibles[][2],
+                  MurInterdit **murInterdits, int *nombreMursInterdits);
+
+void placerRobots(char **grille, int hauteur, int largeur,
+                  MurInterdit *murInterdits, int nombreMursInterdits);
+
 void afficherGrille(char **grille, int hauteur, int largeur, int **MurRandH,
-int **MurRandV,int MurH_Cibles[CIBLES][2], int MurV_Cibles[CIBLES][2]);
-int estPositionValide(char **grille, int hauteur, int largeur, int ligne, int col, MurInterdit *murInterdits, int nombreMursInterdits);
-void murExterieur(int hauteur, int largeur, MurInterdit **murInterdits, int *nombreMursInterdits, int **MurRandH, int **MurRandV);
-    
+                    int **MurRandV, int MurH_Cibles[CIBLES][2],
+                    int MurV_Cibles[CIBLES][2]);
+
+int estPositionValide(char **grille, int hauteur, int largeur, int ligne,
+                      int col);
+
+void murExterieur(int hauteur, int largeur, MurInterdit **murInterdits,
+                  int *nombreMursInterdits, int **MurRandH, int **MurRandV);
+
 #endif
