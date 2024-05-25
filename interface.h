9883385d2b86a9493900d0c4ebe35aac32e26ac7
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-25 19:25:15 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,6 +1,4 @@
 
-
-
 #ifndef Interface_h
 #define Interface_h
 
@@ -25,19 +23,25 @@ typedef struct {
 } Cible;
 
 void NombreJoueurs(int *nb_joueur);
-void choisirRobotCible(char **grille, int hauteur, int largeur, Robot *robot,
+void ChoisirRobotCible(char **grille, int hauteur, int largeur, Robot *robot,
                        Cible *cible);
 void chronometrer(int duree_chrono);
 int choixdifficulte(int niveau_difficulte);
-void choix_player(int **nmbMouv, int m_bJoueur, char **grille, Robot *robot,
+void choix_joueur(int **nmbMouv, int m_bJoueur, char **grille, Robot *robot,
           Cible *cible, int *player);
 int MIN(int My_array[], int len);
 
 void deplacement(Robot *robot, Cible *cible, int direction,
                       MurInterdit *murInterdits, int nombreMursInterdits, char ** grille, int hauteur, int largeur);
 int choix_direction(int direction);
+ void JoueurTour(char **grille, int hauteur, int largeur, Robot *robot, Cible *cible, MurInterdit *mursInterdits, int nombreMursInterdits, int *nmbMouv, int joueurActuel, int *pointsJoueurs, int **MurRandH,
+ int **MurRandV, int MurH_Cibles[CIBLES][2],
+ int MurV_Cibles[CIBLES][2]);
 
+void Points(int *pointsJoueurs, int nb_joueurs, int joueurActuel, int nb_deplacements, int nb_deplacements_effectues, int robotAtteintCible);
 
 
 #endif
+
+
 
