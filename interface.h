68891540a81555c50574562fda330e956db10ed4
Clab-ewan <clabautewa@cy-tech.fr>
2024-05-25 03:02:29 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,23 +1,39 @@
 #ifndef Interface_h
-    #define Interface_h
+#define Interface_h
 
-    #include <stdlib.h>
-    #include <stdio.h>
-    #include <time.h>
-    #include <unistd.h>
-	#include "couleur.h"
+#include "couleur.h"
 
-typedef struct{
-    int nbMouv;
-    int points;
-}Joueur;
-    
-   int NombreJoueurs(int nb_joueur);
-   void choisirRobotCible(char **grille, int hauteur, int largeur, char *robot, char *cible);
-   void chronometrer(int duree_chrono); // Chronométrer
-   int choixdifficulte(int niveau_difficulte);
+#include "creerGrille.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
-   void nombresMouv(int nb_joueur, Joueur **joueur);
-    
+typedef struct {
+  int ligne;
+  int col;
+  char signe;
+} Robot;
+
+typedef struct {
+  int ligne;
+  int col;
+  char signe;
+} Cible;
+
+void NombreJoueurs(int *nb_joueur);
+void choisirRobotCible(char **grille, int hauteur, int largeur, Robot *robot,
+                       Cible *cible);
+void chronometrer(int duree_chrono);
+int choixdifficulte(int niveau_difficulte);
+void choix_player(int **nmbMouv, int m_bJoueur, char **grille, Robot *robot,
+          Cible *cible, int *player);
+int MIN(int My_array[], int len);
+
+void deplacement(Robot *robot, Cible *cible, int direction,
+                      MurInterdit *murInterdits, int nombreMursInterdits, char ** grille);
+int choix_direction();
+
+
+
 #endif
-
