b1e5dcedf17d9d74716bd5740cb57a88f2338baa
Lemobutudu71 <166170828+Lemobutudu71@users.noreply.github.com>
2024-05-26 23:51:30 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,4 +1,3 @@
-
 #ifndef Interface_h
 #define Interface_h
 
@@ -14,6 +13,7 @@ typedef struct {
   int ligne;
   int col;
   char signe;
+  char tampon;
 } Robot;
 
 typedef struct {
@@ -23,24 +23,26 @@ typedef struct {
 } Cible;
 
 void NombreJoueurs(int *nb_joueur);
-void ChoisirRobotCible(char **grille, int hauteur, int largeur, Robot *robot,
+void choisirRobotCible(char **grille, int hauteur, int largeur, Robot *robot,
                        Cible *cible);
 void chronometrer(int duree_chrono);
 int choixdifficulte(int niveau_difficulte);
-void choix_joueur(int **nmbMouv, int m_bJoueur, char **grille, Robot *robot,
-          Cible *cible, int *player);
-int MIN(int My_array[], int len);
+void choix_player(int **nmbMouv, int m_bJoueur, char **grille, Robot *robot,
+                  Cible *cible, int *player);
+int MIN(int tab[], int taille);
+void deplacement(Robot *robot, Cible *cible, int direction, int *MurRandH,
+ int *MurRandV, MurCible *murHCible, MurCible *murVCible,
+ char **grille, int hauteur, int largeur);
 
-void deplacement(Robot *robot, Cible *cible, int direction,
-                      MurInterdit *murInterdits, int nombreMursInterdits, char ** grille, int hauteur, int largeur);
-int choix_direction(int direction);
- void JoueurTour(char **grille, int hauteur, int largeur, Robot *robot, Cible *cible, MurInterdit *mursInterdits, int nombreMursInterdits, int *nmbMouv, int joueurActuel, int *pointsJoueurs, int **MurRandH,
- int **MurRandV, int MurH_Cibles[CIBLES][2],
- int MurV_Cibles[CIBLES][2]);
+int choix_direction();
+
+void JoueurTour(char **grille,  int hauteur, int largeur, Robot *robot, Cible *cible, int *MurRandH, int *MurRandV, MurCible *murHCible, MurCible *murVCible, int *nmbMouv, int joueurActuel, int *pointsJoueurs);
 
 void Points(int *pointsJoueurs, int nb_joueurs, int joueurActuel, int nb_deplacements, int nb_deplacements_effectues, int robotAtteintCible);
 
 void AfficherGagnant(int *pointsJoueurs, int nb_joueurs);
+
+ 
 #endif
 
 
